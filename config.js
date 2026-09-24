1b8f658d60ad3bf71c5e7416e289db42cf827434
christhiant02 <christhianquimi@gmail.com>
2026-09-23 20:42:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-/* Configuración de GemaFinanzy. Completa estos dos valores siguiendo GUIA-PUBLICACION.md.
-   Si los dejas vacíos, la app funciona en modo local (sin inicio de sesión ni sincronización). */
 window.GF_CONFIG = {
-  API_URL: '',
-  GOOGLE_CLIENT_ID: '',
-  CONTACTO: ''
+  API_URL: 'https://script.google.com/macros/s/AKfycbxp4TkX0WANDFeAi8p4oyq54fqH-uyy8nBByRj-YxnonX186y8rlTtAOwIr9Bviy6X8/exec',
+  GOOGLE_CLIENT_ID: '335212272693-q97963o3peg515hmeupei76i3e67293d.apps.googleusercontent.com',
+  CONTACTO: 'Escríbeme a christhianquimi@gmail.com'
 };
